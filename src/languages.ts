17fb90559fa77ad301ce828228614432bf6f9920
Bartu İnce <sonic22mario@hotmail.com>
2024-02-20 23:21:03 +0300

    remove log

--- src/languages.ts
+++ src/languages.ts
@@ -4,7 +4,6 @@ export { KNOWN_LANGUAGES, KNOWN_LANGUAGE_CODES }
 export const langPathRegex = /\/([a-z]{2}-?[A-Z]{0,2})\//
 
 export function getLanguageFromURL(pathname: string) {
-  console.log("pathname:" + pathname)
   const langCodeMatch = pathname.match(langPathRegex)
   const langCode = langCodeMatch ? langCodeMatch[1] : 'en'
   return langCode as (typeof KNOWN_LANGUAGE_CODES)[number]
